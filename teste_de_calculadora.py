e2190c35b46889f89833b781b25cca496211cec2
Ícaro Gomes <136552336+IcaroGomesSousa@users.noreply.github.com>
2026-08-28 21:21:32 -0300

    Add files via upload

---
+++ teste_de_calculadora.py
@@ -0,0 +1,27 @@
+def numeros(): # numeros escolhidos
+    n1 = float (input("escolha o primeiro número: "))
+    n2 = float (input("escolha o segundo núnermo: "))
+    return n1, n2
+
+def escolha():
+    n1, n2 = numeros() #captura de valores
+    operação = input("Você gostaria de somar, multiplicar, dividir ou subtrair?  ")
+    if operação == "somar":    # operação de soma
+        resultado = n1 + n2
+        print (f"O resultado da soma foi:{resultado}")
+
+    elif operação == "multiplicar":    # operação de multiplicação
+          resultado = n1 * n2
+          print (f"O resultado da multiplicação foi:{resultado}")
+
+    elif operação == "dividir":
+         resultado = n1 / n2
+         print(f"O resultado da divisão foi:{resultado}")
+    
+    elif operação == "subtrair":
+         resultado = n1 - n2
+         print(f"O resultado da subtração foi:{resultado}")
+    else:
+        print ("não foi possivel fazer a operação")
+
+escolha()
